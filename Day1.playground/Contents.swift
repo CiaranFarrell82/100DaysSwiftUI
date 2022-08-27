685b587fc2ef282980a9f3f9a27e5b1c612ab1a2
CiaranFarrell82 <ciaran.farrell82@mail.dcu.ie>
2022-08-27 15:17:27 +0100

    Day 1 completed

--- Day1.playground/Contents.swift
+++ Day1.playground/Contents.swift
@@ -1,5 +1,122 @@
 import UIKit
 
-var str = "Hello, playground"
+// Day 1 - variables & constants
 
-// practice pull request
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+// let character = "Daphne"
+
+let character = "Daphne"
+character = "Eloise"
+character = "Francesca"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
+
+// Creating Strings
+
+let actor = "Denzel Washington"
+
+let filename = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let film = "A day in
+the life of an
+Apple engineer"
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+print(actor.count)
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+
+print(filename.hasSuffix(".jpg"))
+
+
+// Storing whole numbers
+
+let score = 10
+
+let big = 100000000
+
+let veryBig = 100_000_000
+
+let reallyBig = 1_00__00___00____00
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+print(score)
+
+var counter = 10
+counter = counter + 5
+
+counter += 5
+print(counter)
+
+counter *= 2
+print(counter)
+counter -= 10
+print(counter)
+counter /= 2
+print(counter)
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+print(120.isMultiple(of: 3))
+
+// How to store decimal numbers
+
+let decimalNumber = 0.1 + 0.2
+print(number)
+
+let a = 1
+let b = 2.0
+let c = a + b
+
+let c = a + Int(b)
+
+let c = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var actorName = "Nicolas Cage"
+name = "John Travolta"
+
+var name = "Nicolas Cage"
+name = 57
+
+var rating = 5.0
+rating *= 2
+
